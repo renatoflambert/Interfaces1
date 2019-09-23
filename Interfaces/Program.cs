418cedbd529284c1ab6b461bd8abce5e55db1482
= <=>
2019-09-23 18:48:45 -0300

    criado sem interface

--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Globalization;
+using Interfaces.Entities;
+using Interfaces.Services;
 
 namespace Interfaces
 {
@@ -6,7 +9,28 @@ namespace Interfaces
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter data rent");
+            Console.Write("Car model: ");
+            string model = Console.ReadLine();
+
+            Console.Write("Pick up (dd/mm/yyyy hh/mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm",CultureInfo.InvariantCulture);
+            Console.Write("Return (dd/mm/yyyy hh/mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            Console.Write("Priece per hour: ");
+            double priceHour = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.Write("Priece per day: ");
+            double priceDay = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+            RentalService rentalService = new RentalService(priceHour, priceDay);
+
+            rentalService.ProcessInvoice(carRental);
+
+            Console.WriteLine("invoice: ");
+            Console.WriteLine(carRental.Invoice);
+                       
         }
     }
 }
